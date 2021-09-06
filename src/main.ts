44ab66fb9cd40154c729bc60f96426aac6988f0a
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-09-06 20:52:52 +0200

    javascript view

--- src/main.ts
+++ src/main.ts
@@ -1,105 +1,13 @@
-import { Plugin, WorkspaceLeaf, ViewCreator } from "obsidian";
-import { obsidianExts } from "./helper";
-import { PlaintextSettings, PlaintextSettingTab, DEFAULT_SETTINGS } from "./settings";
-import PlaintextView from "./view";
+import { Plugin, WorkspaceLeaf } from "obsidian";
+import CodeView from "./view";
 
-/**
- * Plaintext plugin.
- *
- * This plugin allows you to edit specified extensions as plaintext files.
- * It does NOT check if a file is binary or textual!
- *
- * @author dbarenholz
- * @version 0.1.0
- */
-export default class PlaintextPlugin extends Plugin {
-  public settings: PlaintextSettings;
-
-  /**
-   * Code that runs (once) when plugin is loaded.
-   */
+export default class CodeViewPlugin extends Plugin {
   async onload(): Promise<void> {
-    console.log("Obsidian Plaintext: loaded plugin.");
-
-    // Load the settings
-    await this.loadSettings();
-
-    // Add settings tab
-    this.addSettingTab(new PlaintextSettingTab(this.app, this));
-
-    // Do the work
-    this.processExts(this.settings.extensions);
-  }
-
-  /**
-   * Code that runs (once) when the plugin is unloaded.
-   */
-  onunload(): void {
-    console.log("Obsidian Plaintext: unloaded plugin.");
-  }
-
-  /**
-   * Loads the settings.
-   */
-  async loadSettings(): Promise<void> {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  /**
-   * Saves the settings.
-   */
-  async saveSettings(): Promise<void> {
-    await this.saveData(this.settings);
-  }
-
-  /**
-   * Creates a view for a plaintext file.
-   * Plaintext views have <b>NO</b> syntax highlighting or other fancy features!
-   *
-   * @param leaf The leaf to create the view at
-   * @param ext Plaintext extension
-   * @returns Plaintext view
-   */
-  viewCreator: ViewCreator = (leaf: WorkspaceLeaf, ext?: string): PlaintextView => {
-    return new PlaintextView(leaf, ext);
-  };
-
-  /**
-   * Processes the extensions.
-   *
-   * @param exts extensions
-   */
-  processExts = (exts: string[]): void => {
-    if (exts.length == 0) {
-      console.log("Plaintext: No extensions to process.");
-      return;
+    const register = (ext: string) => {
+      this.registerView(ext, (leaf: WorkspaceLeaf, ext?: string)=>{return new CodeView(leaf,ext)});
+      this.registerExtensions([ext], ext);
     }
-
-    for (const ext of exts) {
-      // Disallow using obsidian defaults
-      if (ext in obsidianExts) {
-        if (this.settings.debug) {
-          console.log(`Plaintext: Extension '${ext}' is used by Obsidian already! Don't override Obsidian.`);
-        }
-      }
-
-      // Try to register view
-      try {
-        this.registerView(ext, this.viewCreator);
-      } catch {
-        if (this.settings.debug) {
-          console.log(`Plaintext: Extension '${ext}' already has a view registered, ignoring...`);
-        }
-      }
-
-      // Try to register extension
-      try {
-        this.registerExtensions([ext], ext);
-      } catch {
-        if (this.settings.debug) {
-          console.log(`Plaintext: Extension '${ext}' is already registered, ignoring...`);
-        }
-      }
-    }
-  };
+    register("js");
+    register("css");
+  }
 }
--- src/view.ts
+++ src/view.ts
@@ -1,101 +1,32 @@
-import CodeMirror from "codemirror";
-import { TextFileView, WorkspaceLeaf } from "obsidian";
+import { MarkdownView, WorkspaceLeaf } from "obsidian";
 
-/**
- * The view used for plaintext files.
- * Editing is facilitated with a codemirror instance, with minimal settings.
- *
- * @author dbarenholz
- * @version 0.0.2
- */
-export default class PlaintextView extends TextFileView {
-  // Internal codemirror instance
-  codeMirror: CodeMirror.Editor;
-
+export default class CodeView extends MarkdownView  {
   // Current file extension
   ext: string;
 
-  // Constructor
-  constructor(leaf: WorkspaceLeaf, ext: string) {
-    // Call super
+  constructor(leaf: WorkspaceLeaf,ext: string) {
     super(leaf);
-
-    // Create code mirror instance and add listener to it.
-    // TODO: Check if this theme needs to be added or not
-    // this.codeMirror = CodeMirror(this.contentEl, {
-    //   theme: "obsidian",
-    // });
-    this.codeMirror = CodeMirror(this.contentEl);
-    this.codeMirror.on("changes", this.changed);
-
-    // Save extension
     this.ext = ext;
   }
 
-  /**
-   * Event handler for CodeMirror editor. Requests a save.
-   * @param _ unused
-   * @param __  unused
-   */
-  changed = async (_: CodeMirror.Editor, __: CodeMirror.EditorChangeLinkedList[]): Promise<void> => {
-    this.requestSave();
-  };
-
-  /**
-   * Event handler for resizing a view. Refreshes codemirror.
-   */
-  onResize(): void {
-    this.codeMirror.refresh();
-  }
-
-  /**
-   * Getter for the data in the view.
-   * Called when saving the contents.
-   *
-   * @returns The file contents as string.
-   */
-  getViewData = (): string => {
-    return this.codeMirror.getValue();
-  };
-
-  /**
-   * Setter for the data in the view.
-   * Called when loading file contents.
-   *
-   * If clear is set, then it means we're opening a completely different file.
-   * In that case, you should call clear(), or implement a slightly more efficient
-   * clearing mechanism given the new data to be set.
-   *
-   * @param data
-   * @param clear
-   */
   setViewData = (data: string, clear?: boolean): void => {
-    if (clear) {
-      // Hardcoded MIME type. Everything is plain text.
-      this.codeMirror.swapDoc(CodeMirror.Doc(data, "text/plain"));
-    } else {
-      this.codeMirror.setValue(data);
+    let header = null;
+    switch(this.file.extension) {
+      case "js": header = "/*\n```javascript*/\n"; break;
+      case "css": header = "/*\n```css*/\n"; break;
     }
+    
+    if(header && !data.startsWith(header)) {
+      data = header + data;
+    }   
+    super.setViewData(data, clear);
   };
 
-  // Clearing this particular item is setting the value to an empty string
-  clear = (): void => {
-    this.codeMirror.setValue("");
-    this.codeMirror.clearHistory();
-  };
-
-  // This method doesn't really do much for our usecase...
   canAcceptExtension(extension: string): boolean {
     return extension == this.ext;
   }
 
-  // Returns the extension
   getViewType(): string {
     return this.ext;
-  }
-
-  // Returns the basename of the open file, or "plaintext" if it doesn't exist
-  getDisplayText(): string {
-    return this.file ? this.file.basename : "plaintext (no file)";
   }
 }
